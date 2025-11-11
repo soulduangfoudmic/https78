ba3a82f07c3b2f960a605044b9a8cb018d713fc5
soulduangfoudmic <242961497+soulduangfoudmic@users.noreply.github.com>
2025-11-11 14:20:09 +0000

    Add src/components/Pool/ApyBadge/index.tsx
    Initial commit with selected file from source repository.
    
    Source: https://github.com/SwaprHQ/swapr-dapp

---
+++ src/components/Pool/ApyBadge/index.tsx
@@ -0,0 +1,32 @@
+import { Percent } from '@swapr/sdk'
+
+import styled from 'styled-components'
+
+const Root = styled.div`
+  background: ${props => props.theme.mainPurple};
+  background-blend-mode: overlay, normal;
+  border-radius: 4px;
+  padding: 3px 5px;
+`
+
+const Text = styled.div`
+  font-size: 10px;
+  font-weight: 700;
+  letter-spacing: 0.08em;
+  color: ${props => props.theme.white};
+  text-align: center;
+  line-height: 9px;
+`
+
+interface ApyBadgeProps {
+  apy: Percent
+  upTo?: boolean
+}
+
+export default function ApyBadge({ apy, upTo }: ApyBadgeProps) {
+  return (
+    <Root>
+      <Text>{`${upTo ? 'UP TO' : ''} ${apy.toFixed(2)}% APY`}</Text>
+    </Root>
+  )
+}
